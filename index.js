142d45daf1e8a9c7668f8425f02697101f357a69
Ghulam Muhammad <ghulammuhammadtunio@gmail.com>
2023-05-27 02:11:45 +0500

    add event publishing functionality to event bus

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -17,17 +18,25 @@ app.get("/posts", (req, res) => {
   res.status(200).send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
-  console.log("random id = " + id);
   const { title } = req.body;
-  console.log(req.body);
 
   posts[id] = {
     id,
     title,
   };
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: { id, title },
+  });
+
   res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("event received", req.body.type);
+  res.send({});
 });
 
 app.listen(4000, () => {
